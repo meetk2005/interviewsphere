bd8abb1bb889ec10c6a1b9cd08cb21d03e25cc3d
meetk2005 <meetmatharoo21@gmail.com>
2026-04-22 02:39:12 +0530

    Updated project structure and gitignore

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,25 @@
 import express from "express";
 import path from "path"
+import cors from "cors"
+import {serve} from "inngest/express"
+import { inngest, functions } from "./lib/inngest.js";
+
 import {ENV} from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 const app=express();
 
 // console.log(ENV.PORT)
 // console.log(ENV.DB_URL)
 
 const __dirname=path.resolve();
+
+//middlewares
+app.use(express.json())
+
+//credntials true meaning?? => server allows a brower (our frontend) to include cookies on request
+app.use(cors({origin:ENV.CLIENT_URL || "http://localhost:5173",credentials:true}))
+
+app.use("/api/inngest",serve({client:inngest, functions}))
 
 app.get("/",(req,res)=>{
     res.status(200).json({msg:"hello nile bille."})
@@ -17,7 +30,7 @@ app.get("/health",(req,res)=>{
 })
 
 //make or app ready for deployment
-if(ENV.NODE_ENV==="development"){
+if(ENV.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
     app.get("/{*any}",(req,res)=>{
@@ -25,4 +38,16 @@ if(ENV.NODE_ENV==="development"){
     })
 }
 
-app.listen(ENV.PORT,() => console.log("Server is running on port:",ENV.PORT));
+
+
+
+const startServer=async()=>{
+    try{
+        await connectDB();
+        app.listen(ENV.PORT,() => {
+            console.log("Server is running on port:",ENV.PORT)});
+    }catch(error){
+        console.error("Error starting the server",error)
+    }
+};
+startServer();
---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { ClerkProvider } from '@clerk/react'
+
+// Get key from .env
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+console.log("KEY:", import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </StrictMode>,
+)
